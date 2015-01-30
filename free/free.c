cb5c6e77cb0ecb3899d7448dc19308f420acb166
moran-_d <moran-_d@epitech.eu>
2015-01-30 11:37:05 +0100

    maj malloc, free

--- free/free.c
+++ free/free.c
@@ -5,10 +5,21 @@
 ** Login   <terran_j@epitech.net>
 **
 ** Started on  Mon Jan 26 11:41:48 2015 Julie Terranova
-** Last update Fri Jan 30 11:13:11 2015 moran-_d
+** Last update Fri Jan 30 11:35:34 2015 moran-_d
 */
 
 #include "all.h"
+
+void merge_block(t_zone *zone)
+{
+  if (zone->next != NULL && zone->next->isFree == 1)
+    {
+      zone->size += zone->next->size + sizeof(*zone);
+      if (zone->next->next != NULL)
+	zone->next->next->prev = zone;
+      zone->next = zone->next->next;
+    }
+}
 
 void	free(void *ptr)
 {
@@ -16,12 +27,21 @@ void	free(void *ptr)
 
   if (ptr == NULL)
     return;
-  zone = ptr - sizeof(*zone);
+  zone = (t_zone*)(ptr - sizeof(*zone));
   if (zone->isFree != 0)
     return;
   zone->isFree = 1;
-  if (zone->next->isFree == 1)
+  merge_block(zone);
+  if (zone->prev != NULL && zone->prev->isFree == 1)
     {
-      
+      zone = zone->prev;
+      merge_block(zone);
+    }
+  if (zone->next == NULL)
+    {
+      if (zone->prev != NULL)
+	zone->prev->next = NULL;
+      if (zone->size + sizeof(*zone) > getpagesize())
+	sbrk(getpagesize() * -1);
     }
 }
--- malloc/malloc.c
+++ malloc/malloc.c
@@ -5,7 +5,7 @@
 ** Login   <terran_j@epitech.net>
 **
 ** Started on  Mon Jan 26 11:40:01 2015 Julie Terranova
-** Last update Fri Jan 30 11:10:50 2015 Julie Terranova
+** Last update Fri Jan 30 11:36:39 2015 moran-_d
 */
 
 #include "all.h"
@@ -35,10 +35,10 @@ int	pass_by_me(t_zone **ret, size_t size, t_zone *tmp)
   if ((*ret)->isFree == 0)
     {
       while ((void*)(*ret) + (*ret)->size + size
-	     + (2 * sizeof(t_zone)) >= sbrk(0))
+	     + (2 * sizeof(*ret)) >= sbrk(0))
 	if (sbrk(getpagesize()) == (void*) -1)
 	  return (-1);
-      (*ret)->next = (t_zone*)((void*)(*ret) + sizeof(t_zone) + (*ret)->size);
+      (*ret)->next = (t_zone*)((void*)(*ret) + sizeof(*ret) + (*ret)->size);
       (*ret)->next->prev = (*ret);
       (*ret)->next->next = NULL;
       (*ret) = (*ret)->next;
@@ -46,11 +46,11 @@ int	pass_by_me(t_zone **ret, size_t size, t_zone *tmp)
   else
     {
       if ((*ret)->next == NULL)
-	while ((void*)(*ret) + sizeof(t_zone) + size >= sbrk(0))
+	while ((void*)(*ret) + sizeof(*ret) + size >= sbrk(0))
 	  if (sbrk(getpagesize()) == (void*) -1)
 	    return (-1);
       tmp = (*ret)->next;
-      (*ret)->next = (t_zone*)((void*)(*ret) + sizeof(t_zone) + size);
+      (*ret)->next = (t_zone*)((void*)(*ret) + sizeof(*ret) + size);
       if (tmp != NULL)
 	tmp->prev = (*ret)->next;
       (*ret)->next->prev = (*ret);
@@ -69,11 +69,11 @@ void	*malloc(size_t size)
     return ((void*)ret);
   while (ret->next != NULL &&
 	 (ret->isFree == 0 ||
-	  (ret->isFree == 1 && (ret->size + sizeof(t_zone)) > size)))
+	  (ret->isFree == 1 && (ret->size + sizeof(*ret)) > size)))
     ret = ret->next;
   if (pass_by_me(&ret, size, NULL) == -1)
     return (NULL);
   ret->isFree = 0;
   ret->size = size;
-  return ((void*)ret + sizeof(t_zone));
+  return ((void*)ret + sizeof(*ret));
 }
